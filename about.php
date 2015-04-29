d7a68bf33eb51828f58987a8e9d8013e06c8dd5d
David Garber <daggad95@gmail.com>
2015-04-29 09:41:55 -0700

    hidding comments

--- about.php
+++ about.php
@@ -8,6 +8,8 @@
 		<p class="indent">
 			My parents always made our cakes from scratch. Several of the recipes I use are their originals — some of which were passed down to them by my grandma, who was an amazing baker as well. I have many recipes of my own creation, and a borderline addiction to cake decorating that goes well beyond what my parents or grandparents had. But it’s still their well-worn, dog-eared recipe cards and cookbook pages that I turn to for inspiration when the need arises. It is this combination of time-tested favorites and modern concepts that makes Elefont cakes the unique creations  they are. 
 		</p>
+
+		<!--
 		<h1>Comments from Clients</h1>
 		<?php
 			foreach (file("comments.txt") as $line) {
@@ -38,4 +40,5 @@
 			<input type="submit" value="Leave Comment!"/>
 		</form>
 	</div>
+	-->
 <?php include "footer.php"; ?>
